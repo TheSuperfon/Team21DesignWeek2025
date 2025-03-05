e8f3b3d67f0f1d13b7ed906c2a824cf4273d3980
dexc34 <dexc43@gmail.com>
2025-03-05 16:36:55 -0500

    feedback v1

--- 2DDesignWeek2025Team21/Assets/Scripts/DragDrop.cs
+++ 2DDesignWeek2025Team21/Assets/Scripts/DragDrop.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler
-    , IBeginDragHandler, IEndDragHandler, IDragHandler
+    , IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     //[SerializeField] private Canvas canvas;
 
     [SerializeField] private bool isTile = true;
+    [SerializeField] Image outline;
+    [SerializeField] Image tile;
+    [SerializeField] Canvas dragCanvas;
+    Canvas newDragCanvas;
+    Transform parentToStore;
 
     private RectTransform rectTransform;
     private Vector3 originalPosition;
@@ -34,7 +40,16 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler
     public void OnBeginDrag(PointerEventData eventData)
     {
         canvasGroup.blocksRaycasts = false;
-        canvasGroup.alpha = 0.4f;
+        tile.color = new Color32(56, 200, 171,255);
+        //canvasGroup.alpha = 0.4f;
+
+        //newDragCanvas = Instantiate(dragCanvas, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+
+        //parentToStore = gameObject.transform.parent;
+        //newDragCanvas.transform.parent = parentToStore;
+        //gameObject.transform.parent = newDragCanvas.transform;
+        //newDragCanvas.overrideSorting = true;
+        //newDragCanvas.sortingOrder = 1;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -47,13 +62,18 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (shouldReset && isTile)
+        /*if (shouldReset && isTile)
         {
             transform.position = originalPosition;
-        }
+        }*/
         canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1;
         shouldReset = true;
+
+        tile.color = new Color32(200, 63, 56,255);
+
+        //gameObject.transform.parent = parentToStore;
+        //Destroy(newDragCanvas);
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -63,5 +83,23 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler
             originalPosition = transform.position;
         }
 
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (outline != null)
+        {
+            outline.enabled = true;
+            Debug.Log("mouse hover");
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (outline != null)
+        {
+            outline.enabled = false;
+            Debug.Log("mouse off");
+        }
     }
 }
