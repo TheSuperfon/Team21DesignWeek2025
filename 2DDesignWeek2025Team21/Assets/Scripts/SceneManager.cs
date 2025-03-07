e3dff2970dcc1dd25a45151cbe017bba054835d9
dexc34 <dexc43@gmail.com>
2025-03-07 00:15:33 -0500

    scene manager

--- 2DDesignWeek2025Team21/Assets/Scripts/SceneManager.cs
+++ 2DDesignWeek2025Team21/Assets/Scripts/SceneManager.cs
@@ -5,15 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string sceneName;
+    public void ChangeScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.loadScene(sceneName);
     }
 }
